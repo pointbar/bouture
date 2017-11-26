72a6cd6e5d47e5dd4e5adb362a652acb94c38e15
Travis CI <stephane.langlois@scopyleft.fr>
2017-11-26 18:22:37 +0100

    Create a basic test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const expect = require('chai').expect;
+
+describe('isParisMuseum', function () {
+    it('should return false', function () {
+        expect(false).to.be.false;
+    });
+});
