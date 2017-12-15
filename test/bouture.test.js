308109a86f1f881c3a5e5fda53137e6a80cbf73f
David Bruant <bruant.d@gmail.com>
2017-12-15 15:52:55 +0100

    Adding test related to creating an element with text as content

--- test/bouture.test.js
+++ test/bouture.test.js
@@ -4,6 +4,7 @@ import Bouture from '../bouture.js'
 describe('Bouture.div', () => {
   it('should create an object', () => {
     const b = Bouture.div
+
     expect(Object(b)).to.equal(b)
   })
   it('should create a new element on each access', () => {
@@ -14,6 +15,16 @@ describe('Bouture.div', () => {
 describe('Bouture.div.getElement()', () => {
   it('should return an actual DOM node', () => {
     const b = Bouture.div.getElement()
+
     expect(b).to.be.an.instanceof(HTMLElement)
+  })
+})
+
+describe(`Bouture.div('yo')`, () => {
+  it(`should create a div with text 'yo'`, () => {
+    const div = Bouture.div('yo')
+
+    expect(div.getElement()).to.be.an.instanceof(HTMLElement)
+    expect(div.getElement().textContent).to.equal('yo')
   })
 })
