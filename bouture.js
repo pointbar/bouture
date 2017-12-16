cc4fd812f36a2da1ef79199d4eb3d77ff19b7be0
Travis CI <stephane.langlois@scopyleft.fr>
2017-12-16 23:01:13 +0100

    First try with proxy

--- bouture.js
+++ bouture.js
@@ -1,8 +1,30 @@
-export default {
-  get div () {
-    const div = document.createElement('div')
-    return {
-      getElement: () => div
+const protoDiv = (text) => {
+  const div = document.createElement('div')
+  div.append(text)
+  return {
+    getElement: () => {
+      return div
     }
   }
+}
+
+const handler = {
+  apply: (target, thisArg, args) => {
+    return target(args[0])
+  },
+  get: (target, propKey, receiver) => {
+    const origMethod = target()[propKey]
+    if (typeof propKey === 'symbol') {
+      return target()
+    }
+    return function (...args) {
+      try {
+        return origMethod.apply(this, args)
+      } catch (e) {}
+    }
+  }
+}
+
+export default {
+  get div () { return new Proxy(protoDiv, handler) }
 }
