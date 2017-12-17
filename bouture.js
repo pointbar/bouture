da54274c4d03fe3a20812a7a0359fda41b96ba89
David Bruant <bruant.d@gmail.com>
2017-12-17 22:23:01 +0100

    Ability to create a handful of HTML elements - credit @pointbar

--- bouture.js
+++ bouture.js
@@ -1,14 +1,19 @@
+const Bouture = {}
 
-export default {
-  get div () {
-    const div = document.createElement('div')
-    function branche (text) {
-      div.append(text)
+;['h1', 'h2', 'a', 'input', 'iframe', 'img', 'div'].forEach(name => {
+  Object.defineProperty(Bouture, name, {
+    get: () => {
+      const element = document.createElement(name)
+      function branche (text) {
+        element.append(text)
+        return branche
+      }
+      branche.getElement = () => {
+        return element
+      }
       return branche
     }
-    branche.getElement = () => {
-      return div
-    }
-    return branche
-  }
-}
+  })
+})
+
+export default Bouture
