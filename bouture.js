39aa3b0161d29da0abaf33ebd23f356f0d07430d
Travis CI <stephane.langlois@scopyleft.fr>
2017-12-18 10:13:33 +0100

    Create list with all standard elements

--- bouture.js
+++ bouture.js
@@ -1,9 +1,10 @@
 const Bouture = {}
+const tags = ['a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'cite', 'code', 'col', 'colgroup', 'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'meta', 'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'rtc', 'ruby', 's', 'samp', 'script', 'section', 'select', 'slot', 'small', 'source', 'span', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'u', 'ul', 'var', 'video', 'wbr']
 
-;['h1', 'h2', 'a', 'input', 'iframe', 'img', 'div'].forEach(name => {
-  Object.defineProperty(Bouture, name, {
+tags.forEach(tag => {
+  Object.defineProperty(Bouture, tag, {
     get: () => {
-      const element = document.createElement(name)
+      const element = document.createElement(tag)
       function branche (text) {
         element.append(text)
         return branche
